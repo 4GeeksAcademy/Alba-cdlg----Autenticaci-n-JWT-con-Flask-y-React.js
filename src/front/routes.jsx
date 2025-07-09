05c91c516f22bc1bdbda1179d4e3b3c71707243f
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-07-09 08:00:02 +0000

    eliminado demo.jsx y single.jsx

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,8 +7,6 @@ import {
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Private from "./pages/Private";
@@ -27,8 +25,6 @@ export const router = createBrowserRouter(
 
       {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
       <Route path="/" element={<Home />} />
-      <Route path="/single/:theId" element={<Single />} />  {/* Dynamic route for single items */}
-      <Route path="/demo" element={<Demo />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route
